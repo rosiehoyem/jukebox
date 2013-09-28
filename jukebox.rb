a0cdd74286b9e950e48d0ad5eaf511ab7c06534b
Kyle Shike <kyleshike@gmail.com>
2013-09-28 10:18:14 -0400

    fix minor errors

--- jukebox.rb
+++ jukebox.rb
@@ -1,41 +1,4 @@
 # encoding: utf-8
-# Download Gist: https://gist.github.com/scottcreynolds/e6534b284373efe0ba6e/download
-# Build a Jukebox
- 
-# create a file jukebox.rb
- 
-# When that program is run, it should introduce itself
-# to the user and accept input from the user using the gets command.
- 
-# The jukebox should respond to 4 commands, help, play, list and exit.
- 
-# The help command should output instructions for the user
-# on how to use the jukebox.
- 
-# The list command should output a list of songs that the
-# user can play.
- 
-# the play command should accept a song, either by number/index
-# or name. Once the user has indicated which song they want to
-# play, the jukebox should output 'Playing The Phoenix - 1901'
-# or whatever song name is important.
- 
-# if the user types in exit, the jukebox should say goodbye
-# and the program should shut down.
- 
-# Think about the following things
- 
-# How to keep the program running until the exit command is
-# executed (Hint: Loop maybe? Loop upon a condition)
- 
-# How to normalize the user's input so LIST and list are the
-# same. (Hint, maybe downcase and strip it)
- 
-# How to give the songs an "index" so that when you list them
-# out, you can refer to them by position so the user can just
-# type play 1 and then you find the first song. (Hint, check
-# out a method called each_with_index)
-
 songs = [
 "The Phoenix - 1901",
 "Tokyo Police Club - Wait Up",
@@ -58,6 +21,8 @@ end
 exit = false
 puts "♫   Hello, please enter one of the following commands: ♫"
 puts "help, play, list, exit".center(50)
+
+
 while exit == false
   print "♫  "
   input = gets.chomp.downcase
@@ -69,6 +34,9 @@ while exit == false
     puts "help, play, list, exit"
   when "list"
     songs_with_index(songs)
+
+
+
   when "play"
     puts "Would you like to select song by name or index?"
     name_or_index_response = gets.chomp.downcase
@@ -78,7 +46,7 @@ while exit == false
         songs_with_index(songs)
         puts "Please select the index of the song you want to hear from the list above:"
         index_choice = gets.chomp.downcase
-        puts "Playing #{songs[index_choice.to_i - 1]}"
+        puts "Playing #{sprint_to_screen("hello")songs[index_choice.to_i - 1]}"
         break
       when "name"
         songs_with_index(songs)
@@ -90,6 +58,8 @@ while exit == false
         puts "Please select a name or an index number:"
         name_or_index_response = gets.chomp.downcase
       end
+
+
 
     end
   when "exit"
